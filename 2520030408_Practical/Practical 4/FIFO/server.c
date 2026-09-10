39b3e9d94b10f592a9dac64f2b3076df6f5cc6a9
Jamuna408 <bistajamuna85@gmail.com>
2026-09-10 10:28:50 +0530

    Update server.c

--- 2520030408_Practical/Practical 4/FIFO/server.c
+++ 2520030408_Practical/Practical 4/FIFO/server.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
+int main()
+{
+    char msg[100];
+
+    mkfifo("fifo", 0666);
+
+    int fd = open("fifo", O_RDONLY);
+
+    while (1)
+    {
+        read(fd, msg, sizeof(msg));
+        printf("Client: %s\n", msg);
+        printf("Server: Message received\n");
+    }
+
+    close(fd);
+    return 0;
+}
