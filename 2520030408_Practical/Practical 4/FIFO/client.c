f2cb54fec9540fe6b8e5138acd9dd8b174cfc18a
Jamuna408 <bistajamuna85@gmail.com>
2026-09-10 10:28:33 +0530

    Update client.c

--- 2520030408_Practical/Practical 4/FIFO/client.c
+++ 2520030408_Practical/Practical 4/FIFO/client.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+int main()
+{
+    char msg[100];
+
+    int fd = open("fifo", O_WRONLY);
+
+    printf("Enter message: ");
+    fgets(msg, sizeof(msg), stdin);
+
+    write(fd, msg, sizeof(msg));
+
+    close(fd);
+    return 0;
+}
