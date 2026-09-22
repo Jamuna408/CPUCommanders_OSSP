e1228619bbeea0bee1d3e193230b4cd5be987a53
Jamuna408 <bistajamuna85@gmail.com>
2026-09-22 11:24:25 +0530

    Create server.c

---
+++ 2520030408_Practical/Practical 6/FIFO/server.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+    char msg[100];
+
+    mkfifo("fifo", 0666);
+
+    int fd = open("fifo", O_RDONLY);
+
+    while(1)
+    {
+        read(fd, msg, sizeof(msg));
+        printf("Client: %s\n", msg);
+    }
+
+    close(fd);
+    return 0;
+}
